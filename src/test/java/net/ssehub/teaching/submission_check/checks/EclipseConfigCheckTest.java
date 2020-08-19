777b3dfcb7e988d0f0c6547d46f2771ca03f7285
Adam <adam@sse.uni-hildesheim.de>
2020-08-19 11:31:11 +0200

    Update EclipseConfigCheckTest with Junit 5 features
    Use @DisplayName
    Use assertAll

--- src/test/java/net/ssehub/teaching/submission_check/checks/EclipseConfigCheckTest.java
+++ src/test/java/net/ssehub/teaching/submission_check/checks/EclipseConfigCheckTest.java
@@ -17,12 +17,14 @@ package net.ssehub.teaching.submission_check.checks;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import net.ssehub.teaching.submission_check.ResultMessage;
@@ -35,6 +37,7 @@ public class EclipseConfigCheckTest {
     private static final File TESTDATA = new File("src/test/resources/EclipseConfigCheckTest");
     
     @Test
+    @DisplayName("does not succeed if .classpath file is missing")
     public void missingClasspath() {
         File direcory = new File(TESTDATA, "missingClasspath");
         assertThat("Precondition: directory with test files should exist",
@@ -42,16 +45,18 @@ public class EclipseConfigCheckTest {
         
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("Postcondition: missing classpath should not succeed",
-                check.run(direcory), is(false));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should create an error message",
-                check.getResultMessages(), is(Arrays.asList(
-                        new ResultMessage("eclipse-configuration", MessageType.ERROR, "Does not contain a valid eclipse project")
-                )));
+        assertAll(
+            () -> assertThat("Postcondition: should not succeed", success, is(false)),
+            () -> assertThat("Postcondition: should create an error message", check.getResultMessages(), is(Arrays.asList(
+                    new ResultMessage("eclipse-configuration", MessageType.ERROR, "Does not contain a valid eclipse project")
+                )))
+        );
     }
     
     @Test
+    @DisplayName("does not succeed if .project file is missing")
     public void missingProject() {
         File direcory = new File(TESTDATA, "missingProject");
         assertThat("Precondition: directory with test files should exist",
@@ -59,16 +64,18 @@ public class EclipseConfigCheckTest {
         
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("Postcondition: missing project should not succeed",
-                check.run(direcory), is(false));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should create an error message",
-                check.getResultMessages(), is(Arrays.asList(
-                        new ResultMessage("eclipse-configuration", MessageType.ERROR, "Does not contain a valid eclipse project")
-                )));
+        assertAll(
+            () -> assertThat("Postcondition: should not succeed", success, is(false)),
+            () -> assertThat("Postcondition: should create an error message", check.getResultMessages(), is(Arrays.asList(
+                    new ResultMessage("eclipse-configuration", MessageType.ERROR, "Does not contain a valid eclipse project")
+                )))
+        );
     }
     
     @Test
+    @DisplayName("does not succeed if .project file has an invalid format")
     public void invalidProjectFIle() {
         File direcory = new File(TESTDATA, "invalidProject");
         assertThat("Precondition: directory with test files should exist",
@@ -76,16 +83,18 @@ public class EclipseConfigCheckTest {
         
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("Postcondition: invalid project file should not succeed",
-                check.run(direcory), is(false));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should create an error message",
-                check.getResultMessages(), is(Arrays.asList(
-                        new ResultMessage("eclipse-configuration", MessageType.ERROR, "Does not contain a valid eclipse project")
-                )));
+        assertAll(
+            () -> assertThat("Postcondition: should not succeed", success, is(false)),
+            () -> assertThat("Postcondition: should create an error message", check.getResultMessages(), is(Arrays.asList(
+                    new ResultMessage("eclipse-configuration", MessageType.ERROR, "Does not contain a valid eclipse project")
+                )))
+        );
     }
     
     @Test
+    @DisplayName("succeeds on valid project")
     public void valid() {
         File direcory = new File(TESTDATA, "javaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -93,14 +102,16 @@ public class EclipseConfigCheckTest {
         
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("Postcondition: correct setup should succeed",
-                check.run(direcory), is(true));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should not create any messages",
-                check.getResultMessages(), is(Arrays.asList()));
+        assertAll(
+            () -> assertThat("Postcondition: should succeed", success, is(true)),
+            () -> assertThat("Postcondition: should create no error messages", check.getResultMessages(), is(Arrays.asList()))
+        );
     }
     
     @Test
+    @DisplayName("succeeds on non-Java project if Java projects are not required")
     public void nonJavaProjectAllowed() {
         File direcory = new File(TESTDATA, "nonJavaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -108,14 +119,16 @@ public class EclipseConfigCheckTest {
         
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("Postcondition: a non-java project should succeed when allowed",
-                check.run(direcory), is(true));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should not create any messages",
-                check.getResultMessages(), is(Arrays.asList()));
+        assertAll(
+            () -> assertThat("Postcondition: should succeed", success, is(true)),
+            () -> assertThat("Postcondition: should create no error messages", check.getResultMessages(), is(Arrays.asList()))
+        );
     }
     
     @Test
+    @DisplayName("does not succeed on non-Java project if Java projects are required")
     public void nonJavaProjectNotAllowed() {
         File direcory = new File(TESTDATA, "nonJavaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -124,17 +137,19 @@ public class EclipseConfigCheckTest {
         EclipseConfigCheck check = new EclipseConfigCheck();
         check.setRequireJavaProject(true);
         
-        assertThat("Postcondition: a non-java project should not succeed when not allowed",
-                check.run(direcory), is(false));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should create an error message",
-                check.getResultMessages(), is(Arrays.asList(
-                        new ResultMessage("eclipse-configuration", MessageType.ERROR, "Submission is not a Java project")
-                            .setFile(new File(".project"))
-                )));
+        assertAll(
+            () -> assertThat("Postcondition: should not succeed", success, is(false)),
+            () -> assertThat("Postcondition: should create an error message", check.getResultMessages(), is(Arrays.asList(
+                    new ResultMessage("eclipse-configuration", MessageType.ERROR, "Submission is not a Java project")
+                        .setFile(new File(".project"))
+                )))
+        );
     }
     
     @Test
+    @DisplayName("succeeds on valid Java project if Java projects are required")
     public void validRequiredJavaProject() {
         File direcory = new File(TESTDATA, "javaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -143,14 +158,16 @@ public class EclipseConfigCheckTest {
         EclipseConfigCheck check = new EclipseConfigCheck();
         check.setRequireJavaProject(true);
         
-        assertThat("Postcondition: a java project should succeed when required",
-                check.run(direcory), is(true));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should not create any messages",
-                check.getResultMessages(), is(Arrays.asList()));
+        assertAll(
+            () -> assertThat("Postcondition: should succeed", success, is(true)),
+            () -> assertThat("Postcondition: should create no error messages", check.getResultMessages(), is(Arrays.asList()))
+        );
     }
     
     @Test
+    @DisplayName("succeeds on non-Checkstyle project if Checkstyle projects are not required")
     public void nonCheckstyleProjectAllowed() {
         File direcory = new File(TESTDATA, "javaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -158,14 +175,16 @@ public class EclipseConfigCheckTest {
         
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("Postcondition: a non-Checkstyle project should succeed when allowed",
-                check.run(direcory), is(true));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should not create any messages",
-                check.getResultMessages(), is(Arrays.asList()));
+        assertAll(
+            () -> assertThat("Postcondition: should succeed", success, is(true)),
+            () -> assertThat("Postcondition: should create no error messages", check.getResultMessages(), is(Arrays.asList()))
+        );
     }
     
     @Test
+    @DisplayName("warns for non-Checkstyle project if Checkstyle projects are required")
     public void nonCheckstyleProjectNotAllowed() {
         File direcory = new File(TESTDATA, "javaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -174,17 +193,19 @@ public class EclipseConfigCheckTest {
         EclipseConfigCheck check = new EclipseConfigCheck();
         check.setRequireCheckstyleProject(true);
         
-        assertThat("Postcondition: a non-Checkstyle project should succeed even when not allowed",
-                check.run(direcory), is(true));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should create an error message",
-                check.getResultMessages(), is(Arrays.asList(
-                        new ResultMessage("eclipse-configuration", MessageType.WARNING, "Submission does not have Checkstyle enabled")
-                            .setFile(new File(".project"))
-                )));
+        assertAll(
+            () -> assertThat("Postcondition: should succeed", success, is(true)),
+            () -> assertThat("Postcondition: should create a warning message", check.getResultMessages(), is(Arrays.asList(
+                    new ResultMessage("eclipse-configuration", MessageType.WARNING, "Submission does not have Checkstyle enabled")
+                        .setFile(new File(".project"))
+                )))
+        );
     }
     
     @Test
+    @DisplayName("succeeds on valid Checkstyle project if Checkstyle projects are required")
     public void validRequiredCheckstyleProject() {
         File direcory = new File(TESTDATA, "checkstyleProject");
         assertThat("Precondition: directory with test files should exist",
@@ -193,14 +214,16 @@ public class EclipseConfigCheckTest {
         EclipseConfigCheck check = new EclipseConfigCheck();
         check.setRequireCheckstyleProject(true);
         
-        assertThat("Postcondition: a Checkstyle project should succeed when required",
-                check.run(direcory), is(true));
+        boolean success = check.run(direcory);
         
-        assertThat("Postcondition: should not create any messages",
-                check.getResultMessages(), is(Arrays.asList()));
+        assertAll(
+            () -> assertThat("Postcondition: should succeed", success, is(true)),
+            () -> assertThat("Postcondition: should create no error messages", check.getResultMessages(), is(Arrays.asList()))
+        );
     }
     
     @Test
+    @DisplayName("creates internal error message if reading the .project file fails")
     public void unreadableProjectFile() throws IOException {
         File direcory = new File(TESTDATA, "javaProject");
         assertThat("Precondition: directory with test files should exist",
@@ -214,35 +237,42 @@ public class EclipseConfigCheckTest {
            
             FileUtilsTest.setRigFileOperationsToFail(true);
             
-            assertThat("Postcondition: should not succeed on unreadable file",
-                    check.run(direcory), is(false));
+            boolean success = check.run(direcory);
             
-            assertThat("Postcondition: should create an error message",
-                    check.getResultMessages(), is(Arrays.asList(
-                            new ResultMessage("eclipse-configuration", MessageType.ERROR, "An internal error occurred while checking eclipse project")
-                    )));
+            assertAll(
+                () -> assertThat("Postcondition: should not succeed", success, is(false)),
+                () -> assertThat("Postcondition: should create an error message", check.getResultMessages(), is(Arrays.asList(
+                        new ResultMessage("eclipse-configuration", MessageType.ERROR, "An internal error occurred while checking eclipse project")
+                    )))
+            );
+            
         } finally {
         	FileUtilsTest.setRigFileOperationsToFail(false);
         }
     }
     
     @Test
+    @DisplayName("getters return previously set values")
     public void getters() {
         EclipseConfigCheck check = new EclipseConfigCheck();
         
-        assertThat("should return correct default value",
-                check.getRequireJavaProject(), is(false));
-        assertThat("should return correct default value",
-                check.getRequireCheckstyleProject(), is(false));
+        assertAll("should return default values",
+            () -> assertThat("default value for requires Java projects", check.getRequireJavaProject(), is(false)),
+            () -> assertThat("default value for requires Checkstyle projects", check.getRequireCheckstyleProject(), is(false))
+        );
         
         check.setRequireJavaProject(true);
-        assertThat(check.getRequireJavaProject(), is(true));
-        assertThat(check.getRequireCheckstyleProject(), is(false));
+        assertAll("should return configured values",
+            () -> assertThat("value for requires Java projects", check.getRequireJavaProject(), is(true)),
+            () -> assertThat("value for requires Checkstyle projects", check.getRequireCheckstyleProject(), is(false))
+        );
         
         check.setRequireJavaProject(false);
         check.setRequireCheckstyleProject(true);
-        assertThat(check.getRequireJavaProject(), is(false));
-        assertThat(check.getRequireCheckstyleProject(), is(true));
+        assertAll("should return configured values",
+            () -> assertThat("value for requires Java projects", check.getRequireJavaProject(), is(false)),
+            () -> assertThat("value for requires Checkstyle projects", check.getRequireCheckstyleProject(), is(true))
+        );
     }
     
     @BeforeAll
